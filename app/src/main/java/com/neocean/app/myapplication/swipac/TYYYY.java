a1f63a5cbffe49863f1314d8e816aaede3887b7e
Administrator <1793546039@qq.com>
2019-07-09 11:07:06 +0800

    commit

---
+++ app/src/main/java/com/neocean/app/myapplication/swipac/TYYYY.java
@@ -0,0 +1,20 @@
+package com.neocean.app.myapplication.swipac;
+
+import android.view.View;
+
+/**
+ * User weixn
+ * Date 2019/7/8
+ */
+public class TYYYY {
+
+    public void f(View view){
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Math.abs(1009);
+                return true;
+            }
+        });
+    }
+}
